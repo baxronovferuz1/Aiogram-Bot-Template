13e5a8f78c0dc2df43663bcbceaa0789be02ad7c
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-06-22 16:39:46 +0500

    changed

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,3 +7,19 @@ from loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Assalomu alaykum, {message.from_user.full_name}!")
+
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+# from pyrogram import Client,filters,StopPropagation
+# from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+
+# @Client.on_message(filters.command(["start"]), group=-2)
+# async def start(client, message):
+#     # return
+#     joinButton = InlineKeyboardMarkup([
+#         [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+#         [InlineKeyboardButton(
+#             "Report Bugs 😊", url="https://t.me/aryanvikash")]
+#     ])
+#     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+#     await message.reply_text(welcomed, reply_markup=joinButton)
+#     raise StopPropagation
